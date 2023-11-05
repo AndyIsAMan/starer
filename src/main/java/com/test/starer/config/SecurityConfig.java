2e1ba78a7f54323da73f7b3b97d4c73eb8c033bf
liweilin <liweilin@1.con>
2023-11-05 12:39:47 +0800

    fixed:修复SpringSecurity记住我RemeberMe功能报错问题,注释掉配置文件中的SpringSecurity相关的demo配置

--- src/main/java/com/test/starer/config/SecurityConfig.java
+++ src/main/java/com/test/starer/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.test.starer.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -32,6 +36,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("123456"))
+                .roles("USER", "ADMIN");  // 此处必须要设置角色不然会报错
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();}
 
     @Override
     public void configure(WebSecurity web) throws Exception {
